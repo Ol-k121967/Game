dbc4f25433e8e661224362c52910e69558a00e4c
Ol-k121967 <oleg.koriakoff@yandex.ru>
2022-12-21 17:15:11 +0300

    add variativ answer

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,7 @@ let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
 let answerText = 2;
+let answerNumberText = '';
 
 const orderNumberField = document.getElementById('orderNumberField');
 const answerField = document.getElementById('answerField');
@@ -17,7 +18,8 @@ const answerField = document.getElementById('answerField');
 orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
-document.getElementById('btnRetry').addEventListener('click', function () {
+document.getElementById('btnRetry').addEventListener('click', function () 
+{
     minValue = parseInt(prompt('Минимальное значение числа для игры','0'));
     maxValue = parseInt(prompt('Максимальное значение числа для игры','100'));
     minValue = (minValue || -999 || !isNaN(minValue));
@@ -27,31 +29,109 @@ document.getElementById('btnRetry').addEventListener('click', function () {
                                                       (maxValue >= 999) ? maxValue = 999 : maxValue = maxValue;
     alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
     orderNumber = 1;
+    answerNumberText = '';
     answerNumber  = Math.floor((minValue + maxValue) / 2);
     orderNumberField.innerText = orderNumber;
     answerText = Math.round(Math.random() * 3);
-    switch (answerText) {
-        case 0:
-            answerField.innerText = `Вы загадали число ${answerNumber }?`
-            gameRun = true;
+    switch (answerNumber)  {
+      case 0:
+        answerNumberText = "ноль"
+        break;
+      case 1:
+        answerNumberText = "один"
+        break;
+      case 2:
+          answerNumberText = "два"
           break;
-        case 1:
+      case 3:
+          answerNumberText = "три"
+          break;
+      case 4:
+          answerNumberText = "четыре"
+          break;
+      case 5:
+          answerNumberText = "пять"
+          break;
+      case 6:
+          answerNumberText = "шесть"
+          break;
+      case 7:
+          answerNumberText = "семь"
+          break;
+      case 8:
+          answerNumberText = "восемь"
+          break;
+      case 9:
+          answerNumberText = "девять"
+          break;
+      default:
+        answerNumberText = "девять LR"
+    } 
+    console.log(answerNumber);
+    console.log(answerText);
+    console.log(answerNumberText);
+    switch (answerText)
+   {
+        case 0:
+        if (answerNumber >=0) {
+          if (answerNumber <=9) {
+            answerField.innerText = 'Вы загадали число ' + answerNumberText + "?"; 
+            gameRun = true;
+            break;
+          }
+        }  else {  
+        answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        gameRun = true;
+          break;
+        }
+              case 1:
+          if (answerNumber >=0) {
+            if (answerNumber <=9) {
+              answerField.innerText = 'Надеюсь это число ' + answerNumberText + '?'; 
+              gameRun = true;
+              break;
+            }
+          }  else { 
             answerField.innerText = `Надеюсь это число ${answerNumber }?`
             gameRun = true;
-          break;
+            break;
+          }
         case 2:
-            answerField.innerText = `Неужели это ${answerNumber }?`
+          if (answerNumber >=0) {
+            if (answerNumber <=9) {
+              answerField.innerText = 'Неужели это ' + answerNumberText + '?'; 
+              gameRun = true;
+              break;
+            }
+          }  else { 
+            answerField.innerText = `Неужели это ${answerNumber }?`;
             gameRun = true;
+            break;
+          }
        default:
-        answerField.innerText = `Как насчет ${answerNumber }?`
-        gameRun = true;
-      }
-   
-})
+        if (answerNumber >=0) {
+          if (answerNumber <=9) {
+            answerField.innerText = 'Как насчет ' + answerNumberText +'?'; 
+            gameRun = true;
+            break;
+          }
+        }  else { 
+          answerField.innerText = `Как насчет ${answerNumber }?`;
+          gameRun = true;
+          break;
+        }
+    }
+
+}
+//console.log(answerNumber);
+//console.log(answerText);
+//console.log(answerNumberText);
 
 document.getElementById('btnOver').addEventListener('click', function () {
-    if (gameRun){
-        if (minValue === maxValue){
+    if (gameRun)
+    {
+        if (minValue === maxValue)
+        {
             const phraseRandom = Math.round( Math.random());
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
@@ -59,7 +139,8 @@ document.getElementById('btnOver').addEventListener('click', function () {
 
             answerField.innerText = answerPhrase;
             gameRun = false;
-        } else {
+        } else 
+        {
             minValue = answerNumber  + 1;
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
